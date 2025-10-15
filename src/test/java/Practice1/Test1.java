659e370b9adb81397719c19553dc9aea95e78fb8
omrfr <omrfr@Farhan>
2025-10-15 07:31:46 +0530

    Committed

--- src/test/java/Practice1/Test1.java
+++ src/test/java/Practice1/Test1.java
@@ -1,5 +1,14 @@
 package Practice1;
 
+import org.testng.annotations.Test;
+
 public class Test1 {
+	
+	@Test
+	public void AppleTest()
+	{
+		System.out.println("Apple");
+	}
+	
 
 }
--- src/test/java/Practice2/Test2.java
+++ src/test/java/Practice2/Test2.java
@@ -1,5 +1,12 @@
 package Practice2;
 
+import org.testng.annotations.Test;
+
 public class Test2 {
 
+	@Test
+	public void OrangeTest()
+	{
+		System.out.println("Orange");
+	}
 }
