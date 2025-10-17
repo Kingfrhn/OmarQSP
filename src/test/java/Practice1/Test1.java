4bf70d898af7d49d96b6df1cdc10a599bf49bf04
omrfr <omrfr@Farhan>
2025-10-17 07:41:49 +0530

    Committed

--- src/test/java/Practice1/Test1.java
+++ src/test/java/Practice1/Test1.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 
 public class Test1 {
 	
-	@Test
+	@Test(groups = "Smoke")
 	public void AppleTest()
 	{
 		System.out.println("Apple");
--- src/test/java/Practice2/Test2.java
+++ src/test/java/Practice2/Test2.java
@@ -4,7 +4,7 @@ import org.testng.annotations.Test;
 
 public class Test2 {
 
-	@Test
+	@Test(groups = "Regression")
 	public void OrangeTest()
 	{
 		System.out.println("Orange");
